7778f69a480d48960bf7305e5b94be8423af0ee3
aleksandra99-sys <aleksandrajanjic442@gmail.com>
2024-07-09 16:51:53 +0200

    I have no idea at this point

--- PokemonReviewApp/Model/Review.cs
+++ PokemonReviewApp/Model/Review.cs
@@ -3,7 +3,8 @@
     public class Review
     {
         public int Id { get; set; }
-        public int Title { get; set; }
+        public int Rating { get; set; }
+        public string Title { get; set; }
         public string Text { get; set; }
         public Reviewer Reviewer { get; set; }
         public Pokemon Pokemon { get; set; }
