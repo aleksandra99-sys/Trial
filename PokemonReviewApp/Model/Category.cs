7ad03b320a10121abedf6762bb9737ccbf5e1089
aleksandra99-sys <aleksandrajanjic442@gmail.com>
2024-07-09 12:54:04 +0200

    Ispravka prethodnog commit za dodate modele

--- PokemonReviewApp/Model/Category.cs
+++ PokemonReviewApp/Model/Category.cs
@@ -1,9 +1,8 @@
-﻿namespace PokemonReviewApp
+﻿namespace PokemonReviewApp.Model
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 }
--- PokemonReviewApp/Model/Review.cs
+++ PokemonReviewApp/Model/Review.cs
@@ -1,4 +1,4 @@
-﻿namespace PokemonReviewApp
+﻿namespace PokemonReviewApp.Model
 {
     public class Review
     {
