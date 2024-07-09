485e8b158c1ea41561e7ecc41373c8ea18b7ba38
aleksandra99-sys <aleksandrajanjic442@gmail.com>
2024-07-09 12:16:55 +0200

    Dodavanje

---
+++ PokemonReviewApp/Model/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Model
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+    }
+}
---
+++ PokemonReviewApp/Model/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Model
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
