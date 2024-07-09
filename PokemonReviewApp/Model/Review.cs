bc1e40b99987d8f3ab29b36bc4746f6c8629ec64
aleksandra99-sys <aleksandrajanjic442@gmail.com>
2024-07-09 13:41:12 +0200

    Adding relationships

--- PokemonReviewApp/Model/Review.cs
+++ PokemonReviewApp/Model/Review.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public int Title { get; set; }
         public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
