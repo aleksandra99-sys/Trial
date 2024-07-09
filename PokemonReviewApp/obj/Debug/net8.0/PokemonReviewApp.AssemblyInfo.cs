646e60bf943618a75b73366f8fc683ee46f80d69
aleksandra99-sys <aleksandrajanjic442@gmail.com>
2024-07-09 17:04:47 +0200

    This shit ain't working for me

--- PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1553ce78fc5639c8baa783718cb65ab39cae9b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7778f69a480d48960bf7305e5b94be8423af0ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
